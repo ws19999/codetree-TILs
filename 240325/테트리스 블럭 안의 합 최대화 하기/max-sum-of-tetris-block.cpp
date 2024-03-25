d764d2f7f65a073b754c259c82197ec9b1a1604b
Woosung <127220113+ws19999@users.noreply.github.com>
2024-03-25 16:59:14 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240325/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
+++ 240325/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.cpp
@@ -1,57 +1,26 @@
 #include <iostream>
 using namespace std;
 int map[200][200],n,m,ans=0;
-void block11(int x,int y)
+int dx[]={-1,0,1,0};
+int dy[]={0,-1,0,1};
+bool check[200][200]={false};
+void dfs(int i,int j,int sums,int depth)
 {
-    ans=max(ans,map[x][y]+map[x][y+1]+map[x][y+2]+map[x][y+3]);
-}
-void block12(int x,int y)
-{
-    ans=max(ans, map[x][y]+map[x+1][y]+map[x+2][y]+map[x+3][y]);
-}
-void block2(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x+1][y]+map[x][y+1]+map[x+1][y+1]);
-}
-void block31(int x, int y)
-{
-    ans=max(ans,map[x][y]+map[x][y-1]+map[x-1][y-1]+map[x-2][y-1]);
-}
-void block32(int x, int y)
-{
-    ans=max(ans,map[x][y]+map[x-1][y]+map[x-1][y+1]+map[x-1][y+2]);
-}
-void block33(int x, int y)
-{
-    ans=max(ans,map[x][y]+map[x][y+1]+map[x+1][y+1]+map[x+2][y+1]);
-}
-void block34(int x, int y)
-{
-    ans=max(ans,map[x][y]+map[x+1][y]+map[x+1][y-1]+map[x+1][y-2]);
-}
-void block41(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x+1][y]+map[x+1][y+1]+map[x+2][y+1]);
-}
-void block42(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x][y+1]+map[x-1][y+1]+map[x-1][y+2]);
-}
-void block51(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x+1][y]+map[x+2][y]+map[x+1][y+1]);
-}
-void block52(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x][y+1]+map[x][y+2]+map[x+1][y+1]);
-}
-void block53(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x+1][y]+map[x+2][y]+map[x+1][y-1]);
-}
-void block54(int x,int y)
-{
-    ans=max(ans,map[x][y]+map[x][y+1]+map[x][y+2]+map[x-1][y+1]);
+    if(depth==4)
+    {
+        ans=max(ans,sums);
+        return;
+    }
+    for(int k=0;k<4;k++)
+    {
+        int nextx=i+dx[k];
+        int nexty=j+dy[k];
+        if(nextx<0 || nextx>=n || nexty<0 || nexty>=m)continue;
+        if(check[nextx][nexty])continue;
+        check[nextx][nexty]=true;
+        dfs(nextx,nexty,sums+map[nextx][nexty],depth+1);
+        check[nextx][nexty]=false;
+    }
 }
 int main() {
     cin>>n>>m;
@@ -66,19 +35,13 @@ int main() {
     {
         for(int j=0;j<m;j++)
         {
-            if(j+3<m)block11(i,j);
-            if(i+3<n)block12(i,j);
-            if(i+1<n && j+1<m)block2(i,j);
-            if(i-2>0 && j-1>0)block31(i,j);
-            if(i-1>0 && j+2<m)block32(i,j);
-            if(i+2<n && j+1<m)block33(i,j);
-            if(i+2<n && j-2>0)block34(i,j);
-            if(i+2<n && j+1<m)block41(i,j);
-            if(i-1>0 && j+2<m)block42(i,j);
-            if(i+2<n && j+1<m)block51(i,j);
-            if(i+1<n && j+2<m)block52(i,j);
-            if(i+2<n && j-1>0)block53(i,j);
-            if(i-1>0 && j+2<m)block54(i,j);
+            check[i][j]=true;
+            dfs(i,j,map[i][j],1);
+            check[i][j]=false;
+            if(i+2<n && j+1<m)ans=max(ans,map[i][j]+map[i+1][j]+map[i+2][j]+map[i+1][j+1]);
+            if(i+1<n && j+2<m)ans=max(ans,map[i][j]+map[i][j+1]+map[i][j+2]+map[i+1][j+1]);
+            if(i+2<n && j-1>0)ans=max(ans,map[i][j]+map[i+1][j]+map[i+2][j]+map[i+1][j-1]);
+            if(i-1>0 && j+2<m)ans=max(ans,map[i][j]+map[i][j+1]+map[i][j+2]+map[i-1][j+1]);
         }
     }
     cout<<ans;
