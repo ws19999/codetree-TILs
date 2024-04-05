9a3a51802a9a15194e006f42f32bf8ce319bd50c
Woosung <127220113+ws19999@users.noreply.github.com>
2024-04-05 21:13:57 +0900

    Title: 공약수, Time: 149ms, Memory: 25MB, Status: Passed - CodeTree

--- 240405/공약수/common-divisor.py
+++ 240405/공약수/common-divisor.py
@@ -1,13 +1,5 @@
 n=int(input())
 lists=list(map(int,input().split()))
-minn=100000
-for i in lists:
-    if(minn>i):
-        minn=i
-for i in range(1,minn+1):
-    a=True
-    for j in lists:
-        if(j%i!=0):
-            a=False
-    if(a):
+for i in range(1,min(lists)+1):
+    if all(number%i==0 for number in lists):
         print(i)
