4435cc3154616b52c350ba14d2160884f951934e
Woosung <127220113+ws19999@users.noreply.github.com>
2024-04-10 15:12:08 +0900

    Title: 디버깅, Time: 23ms, Memory: 0MB, Status: Passed - CodeTree

--- 240410/디버깅/debugging.cpp
+++ 240410/디버깅/debugging.cpp
@@ -1,55 +1,55 @@
 #include <iostream>
 using namespace std;
 int ladder[31][11];
-int n,m,h,ans=4;
-void dfs(int depth)
+int n, m, h, ans = 4;
+void dfs(int depth,int last)
 {
-    if(depth==ans)return;
-    bool possible=true;
-    for(int i=1;i<=n;i++)
+    if (depth == ans)return;
+    bool possible = true;
+    for (int i = 1; i <= n; i++)
     {
-        int num=i;
-        for(int j=1;j<=h;j++)
+        int num = i;
+        for (int j = 1; j <= h; j++)
         {
-            if(ladder[j][num-1])num--;
-            else if(ladder[j][num])num++;
+            if (ladder[j][num - 1])num--;
+            else if (ladder[j][num])num++;
         }
-        if(num!=i)
+        if (num != i)
         {
-            possible=false;
+            possible = false;
             break;
         }
     }
-    if(possible)
+    if (possible)
     {
-        ans=min(ans,depth);
+        ans = min(ans, depth);
         return;
     }
-    if(depth==3)return;
-    for(int i=1;i<=h;i++)
+    if (depth == 3)return;
+    for (int i = last; i <= h; i++)
     {
-        for(int j=1;j<n;j++)
+        for (int j = 1; j < n; j++)
         {
-            if(!ladder[i][j] && !ladder[i][j-1])
+            if (!ladder[i][j] && !ladder[i][j - 1])
             {
-                ladder[i][j]=1;
-                dfs(depth+1);
-                ladder[i][j]=0;
+                ladder[i][j] = 1;
+                dfs(depth + 1,i);
+                ladder[i][j] = 0;
             }
         }
     }
 }
 int main() {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    cin>>n>>m>>h;
-    for(int i=0;i<m;i++)
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> n >> m >> h;
+    for (int i = 0; i < m; i++)
     {
-        int a,b;
-        cin>>a>>b;
-        ladder[a][b]=1;
+        int a, b;
+        cin >> a >> b;
+        ladder[a][b] = 1;
     }
-    dfs(0);
-    if(ans==4)cout<<-1;
-    else cout<<ans;
+    dfs(0,1);
+    if (ans == 4)cout << -1;
+    else cout << ans;
     return 0;
 }
