a8f2b146b61b6d5c40922bac938a0acb410491cd
Woosung <127220113+ws19999@users.noreply.github.com>
2024-04-20 21:55:02 +0900

    Title: 마법의 숲 탐색, Time: 10ms, Memory: 0MB, Status: Passed - CodeTree

--- 240420/마법의 숲 탐색/magical-forest-exploration.cpp
+++ 240420/마법의 숲 탐색/magical-forest-exploration.cpp
@@ -1,39 +1,13 @@
 #include <iostream>
 #include <climits>
+#include <deque>
 using namespace std;
 int map[74][73];
 int dx[] = { -1,0,1,0 };
 int dy[] = { 0,1,0,-1 };
-bool check[74][73];
+int check[74][73];
 int maximum = 0;
 int R, C, K, ans = 0;
-void dfs(int x, int y)
-{
-	maximum = max(maximum, x);
-	for (int k = 0; k < 4; k++)
-	{
-		int nextx = x + dx[k];
-		int nexty = y + dy[k];
-		if (nextx >= 1 && nexty >= 1 && nextx <= R+2 && nexty <= C)
-		{
-			if (!check[nextx][nexty])
-			{
-				if (map[x][y] > 1000 && map[nextx][nexty])
-				{
-					check[nextx][nexty] = true;
-					dfs(nextx, nexty);
-					check[nextx][nexty] = false;
-				}
-				else if(map[x][y]==map[nextx][nexty] || map[nextx][nexty]==INT_MAX-map[x][y])
-				{
-					check[nextx][nexty] = true;
-					dfs(nextx, nexty);
-					check[nextx][nexty] = false;
-				}
-			}
-		}
-	}
-}
 int main(void)
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -82,9 +56,32 @@ int main(void)
 		map[x][y] = i;
 		for (int k = 0; k < 4; k++)map[x + dx[k]][y + dy[k]] = i;
 		map[x + dx[d]][y + dy[d]] = INT_MAX-i;
-		check[x][y] = true;
-		dfs(x, y);
-		check[x][y] = false;
+		check[x][y] = 1;
+		deque<pair<int,int>> dq;
+		dq.push_back(make_pair(x, y));
+		while (!dq.empty())
+		{
+			x = dq.front().first;
+			y = dq.front().second;
+			maximum = max(maximum, x);
+			dq.pop_front();
+			for (int k = 0; k < 4; k++)
+			{
+				int nextx = x + dx[k];
+				int nexty = y + dy[k];
+				if (nextx >= 1 && nextx <= R + 2 && nexty >= 1 && nexty <= C)
+				{
+					if (check[nextx][nexty] != i)
+					{
+						if ((map[x][y] > 10000 && map[nextx][nexty]) || (map[x][y] == map[nextx][nexty] || map[nextx][nexty] == INT_MAX - map[x][y]) )
+						{
+							check[nextx][nexty] = i;
+							dq.push_back(make_pair(nextx, nexty));
+						}
+					}
+				}
+			}
+		}
 		ans += maximum-2;
 	}
 	cout << ans;
