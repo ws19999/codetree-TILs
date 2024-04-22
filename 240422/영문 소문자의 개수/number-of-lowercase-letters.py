dbde3bbf251aedd6cb97432bb5538ec7c17910c8
Woosung <127220113+ws19999@users.noreply.github.com>
2024-04-22 22:21:18 +0900

    Title: 영문 소문자의 개수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240422/영문 소문자의 개수/number-of-lowercase-letters.py
+++ 240422/영문 소문자의 개수/number-of-lowercase-letters.py
@@ -5,8 +5,8 @@ strings=list(input().split())
 for a in strings:
     if(ord(a)>ord('z') or ord(a)<ord('a')):
         finished=True
-    if(not finished):
-        lists[ord(a)-ord('a')]+=1
+        break
+    lists[ord(a)-ord('a')]+=1
 for i in range(ord('z')-ord('a')):
     if(lists[i]>0):
         print(f"{chr(i+ord('a'))} : {lists[i]}")
