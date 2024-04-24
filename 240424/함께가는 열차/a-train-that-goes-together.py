829f6b128f0402bd71dbabd8fab5efa985c10f16
Woosung <127220113+ws19999@users.noreply.github.com>
2024-04-24 22:40:22 +0900

    Title: 함께가는 열차, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240424/함께가는 열차/a-train-that-goes-together.py
@@ -0,0 +1,12 @@
+N=int(input())
+cnt=1
+lists=[]
+for _ in range(N):
+    a,b=map(int,input().split())
+    lists.append(b)
+speed=lists[-1]
+for i in range(N-2,-1,-1):
+    if(i<speed):
+        speed=i
+        cnt+=1
+print(cnt)
