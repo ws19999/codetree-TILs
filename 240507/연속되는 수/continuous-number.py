fa7cdd9be8bc867c53c8d95c5696a4b4666abfd6
Woosung <127220113+ws19999@users.noreply.github.com>
2024-05-07 23:14:44 +0900

    Title: 연속되는 수, Time: 130ms, Memory: 32MB, Status: Wrong Answer - CodeTree

---
+++ 240507/연속되는 수/continuous-number.py
@@ -0,0 +1,24 @@
+check=[0 for _ in range(1000001)]
+numbers=[]
+N=int(input())
+seq=[]
+for _ in range(N):
+    num=int(input())
+    if(check[num]==0):
+        check[num]=1
+        numbers.append(num)
+    seq.append(num)
+ans=0
+for num in numbers:
+    temp=0
+    last=0
+    for i in range(0,N):
+        if(seq[i]==num):
+            continue
+        if(last==seq[i]):
+            temp+=1
+        else:
+            ans=max(ans,temp)
+            temp=1
+            last=seq[i]
+print(ans)
