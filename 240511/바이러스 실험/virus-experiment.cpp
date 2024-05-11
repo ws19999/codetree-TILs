9c854ae4640fe536ab234d5cc5300e60394c62d1
Woosung <127220113+ws19999@users.noreply.github.com>
2024-05-11 12:15:36 +0900

    Title: 바이러스 실험, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240511/바이러스 실험/virus-experiment.cpp
+++ 240511/바이러스 실험/virus-experiment.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int n,m,k;
 int add_nutrient[10][10];
 int next_nutrient[10][10]={0};
-priority_queue<tuple<int,int,int>> virus;
+deque<tuple<int,int,int>> virus;
 int nutrient[10][10];
 int dx[]={-1,-1,-1,0,1,1,1,0};
 int dy[]={-1,0,1,1,1,0,-1,-1};
@@ -26,17 +26,17 @@ int main() {
     {
         int r,c,age;
         cin>>r>>c>>age;
-        virus.push(make_tuple(-age,r-1,c-1));
+        virus.push_back(make_tuple(-age,r-1,c-1));
     }
     for(int tc=1;tc<=k;tc++)
     {
-        priority_queue<tuple<int,int,int>> nextvirus;
-        while(!virus.empty())
+        deque<tuple<int,int,int>> nextvirus;
+        for(int i=0;i<m;i++)
         {
-        int age=-get<0>(virus.top());
-        int x=get<1>(virus.top());
-        int y=get<2>(virus.top());
-        virus.pop();
+        int age=-get<0>(virus.front());
+        int x=get<1>(virus.front());
+        int y=get<2>(virus.front());
+        virus.pop_front();
         if(age<=nutrient[x][y])
         {
             nutrient[x][y]-=age;
@@ -49,15 +49,21 @@ int main() {
                     int nexty=y+dy[k];
                     if(nextx>=0 && nextx<n && nexty>=0 && nexty<n)
                     {
-                        nextvirus.push(make_tuple(-1,nextx,nexty));
+                        nextvirus.push_back(make_tuple(-1,nextx,nexty));
                     }
                 }
             }
-            nextvirus.push(make_tuple(-age,x,y));
+            virus.push_back(make_tuple(-age,x,y));
         }
         else
         {
             next_nutrient[x][y]+=age/2;
+        }
+        while(!nextvirus.empty())
+        {
+            m++;
+            virus.push_front(nextvirus.front());
+            nextvirus.pop_front();
         }
         }
         for(int i=0;i<n;i++)
@@ -68,7 +74,6 @@ int main() {
                 next_nutrient[i][j]=add_nutrient[i][j];
             }
         }
-        virus=nextvirus;
     }
     cout<<virus.size();
     return 0;
