c7f4764185a86edafd57237ca9449d07b226c136
Woosung <127220113+ws19999@users.noreply.github.com>
2024-05-13 21:27:42 +0900

    Title: 숫자 최대로 더하기, Time: 148ms, Memory: 34MB, Status: Passed - CodeTree

--- 240513/숫자 최대로 더하기/add-max-number.py
+++ 240513/숫자 최대로 더하기/add-max-number.py
@@ -2,7 +2,5 @@ n=int(input())
 lists=list(map(int,input().split()))
 lists.sort()
 for i in range(n-1):
-    lists[n-i-1]=lists[0]/2+lists[n-i-1]
-    del lists[0]
-    lists.sort()
-print(f'{lists[0]:.1f}')
+    lists[n-1]=lists[i]/2+lists[n-1]
+print(f'{lists[n-1]:.1f}')
