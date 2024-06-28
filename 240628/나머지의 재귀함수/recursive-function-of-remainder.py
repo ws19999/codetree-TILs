725a6c93443782aef221a36554524c10fba4857c
Woosung <127220113+ws19999@users.noreply.github.com>
2024-06-28 12:56:42 +0900

    Title: 나머지의 재귀함수, Time: 100ms, Memory: 24MB, Status: Passed - CodeTree

--- 240628/나머지의 재귀함수/recursive-function-of-remainder.py
+++ 240628/나머지의 재귀함수/recursive-function-of-remainder.py
@@ -1,9 +1,11 @@
 n=int(input())
+table=[-1 for _ in range(101)]
 def recursive(num):
-    if(num==1):
-        return 2
-    elif(num==2):
-        return 4
-    else:
-        return (recursive(num-1)*recursive(num-2))%100
-print(recursive(n))
+    if(num==1 or num==2):
+        table[1]=2
+        table[2]=4
+        return
+    recursive(num-1)
+    table[num]=(table[num-2]*table[num-1])%100
+recursive(n)
+print(table[n])
