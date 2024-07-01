25da170b0ebe6cc0a486a69ad7e67ba96a4bac39
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-01 23:09:32 +0900

    Title: 문자열 분해, Time: 225ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240701/문자열 분해/string-decomposition.py
@@ -0,0 +1,31 @@
+str=input()
+cnt=0
+table=[0 for _ in range(len(str))]
+lists=['a','b','c','d','e']
+while(True):
+    pos=False
+    s=0
+    temps=[]
+    for i in range(len(str)):
+        if(table[i]==1):
+            continue
+        if(str[i]==lists[s]):
+            temps.append(i)
+            s+=1
+            if(s==5):
+                pos=True
+                for j in temps:
+                    table[j]=1
+                temps=[]
+                s=0
+    if(pos):
+        cnt+=1
+    elif(s>0):
+        cnt=-1
+        break
+    else:
+        break
+if(cnt==0):
+    print(-1)
+else:
+    print(cnt)
