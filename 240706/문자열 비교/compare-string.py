c0fae1b0b558a8f1afae2572724bdc103f5578fc
Woosung <127220113+ws19999@users.noreply.github.com>
2024-07-06 00:47:32 +0900

    Title: 문자열 비교, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240706/문자열 비교/compare-string.py
@@ -0,0 +1,11 @@
+sets={""}
+n,m=map(int,input().split())
+정답=0
+for _ in range(n):
+    str=input()
+    sets.add(str)
+for _ in range(n):
+    str=input()
+    if str in sets:
+        정답+=1
+print(정답)
