d0cffa49139c8d7f9bc37181829388bfc7a33534
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-06 09:32:45 +0900

    Title: 공백 넣어서 출력, Time: 97ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240806/공백 넣어서 출력/insert-space-and-print.kt
@@ -0,0 +1,7 @@
+fun main()
+{
+    println("score          subject          name")
+    println("\n  100             math           sam")
+    println("   88          science          paul")
+    println("   96         language          jack")
+}
