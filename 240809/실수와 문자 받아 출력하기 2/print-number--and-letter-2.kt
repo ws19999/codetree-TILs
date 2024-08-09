5a1bd4c8d56e1795ba8b4a31eabfec4d64c75c92
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-09 10:53:14 +0900

    Title: 실수와 문자 받아 출력하기 2, Time: 133ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 240809/실수와 문자 받아 출력하기 2/print-number--and-letter-2.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    val s=readLine()!!
+    val x=readLine()!!.toDouble()
+    val y=readLine()!!.toDouble()
+    println(String.format("%.2f",y))
+    println(String.format("%.2f",x))
+    println(s)
+}
