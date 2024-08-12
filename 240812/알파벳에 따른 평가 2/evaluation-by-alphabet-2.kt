5a200ff400d06bc34c30aa98450e3c0212865ecc
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-12 11:39:10 +0900

    Title: 알파벳에 따른 평가 2, Time: 138ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240812/알파벳에 따른 평가 2/evaluation-by-alphabet-2.kt
@@ -0,0 +1,10 @@
+fun main()
+{
+    val char = readLine()!![0]
+    if(char=='S')print("Superior")
+    else if(char=='F')print("Fantastic")
+    else if(char=='G')print("Good")
+    else if(char=='U')print("Usually")
+    else if(char=='E')print("Effort")
+    else print("Failure")
+}
