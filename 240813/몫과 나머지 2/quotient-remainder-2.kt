34775e018cfcfafcb6c20677a634e42ed213a7d9
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-13 10:32:52 +0900

    Title: 몫과 나머지 2, Time: 122ms, Memory: 11MB, Status: Passed - CodeTree

--- 240813/몫과 나머지 2/quotient-remainder-2.kt
+++ 240813/몫과 나머지 2/quotient-remainder-2.kt
@@ -2,13 +2,7 @@ fun main()
 {
     val br=System.`in`.bufferedReader()
     val bw=System.`out`.bufferedWriter()
-    var (x,y)=br.readLine().split(" ").map{it.toInt()}
-    if(y>x)
-    {
-        var temp=y
-        y=x
-        x=temp
-    }
+    val (x,y)=br.readLine().split(" ").map{it.toInt()}
     bw.write("${x/y}---${x%y}")
     bw.close()
     br.close()
