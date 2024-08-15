8b02d332101281e4760bcdfea171373165fdf7ee
Woosung <127220113+ws19999@users.noreply.github.com>
2024-08-15 11:47:48 +0900

    Title: 점수별 랭크, Time: 143ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 240815/점수별 랭크/rank-by-score.kt
@@ -0,0 +1,9 @@
+fun main()
+{
+    val n=readLine()!!.toInt()
+    if(n>=90)print("S")
+    else if(n>=80)print("A")
+    else if(n>=70)print("B")
+    else if(n>=60)print("C")
+    else print("F")
+}
