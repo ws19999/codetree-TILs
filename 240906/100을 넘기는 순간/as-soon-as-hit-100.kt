eae1b00561ba257f196b48bb02282530061962d3
Woosung <127220113+ws19999@users.noreply.github.com>
2024-09-06 16:46:41 +0900

    Title: 100을 넘기는 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240906/100을 넘기는 순간/as-soon-as-hit-100.kt
@@ -0,0 +1,17 @@
+fun main()
+{
+    val n=readLine()!!.toInt()
+    var sums=0
+    var finished=false
+    val temp=readLine()!!.split(" ").map{it.toInt()}
+    for(i in 0..n-1)
+    {
+        sums+=temp[i]
+        if(temp[i]>=100)
+        {
+            println(sums)
+            println("${String.format("%.1f",sums.toDouble()/(i+1))}")
+            break
+        }
+    }
+}
