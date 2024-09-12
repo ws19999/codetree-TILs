0dda6d1ebe96cedd9d2646aa881aa1af0680ab99
Woosung <127220113+ws19999@users.noreply.github.com>
2024-09-12 12:13:01 +0900

    Title: 약수와 배수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240912/약수와 배수/divisor-and-multiple.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    val n=readLine()!!.toInt()
+    val nums=readLine()!!.split(" ").map{it.toInt()}
+    val k=readLine()!!.toInt()
+    var 약수=0
+    var 배수=0
+    for(i in nums)
+    {
+        if(k%i==0)약수+=i
+        if(i%k==0)배수+=i
+    }
+    println(약수)
+    println(배수)
+}
