d9bec00163026286cf576ed7b102ba23f6487ac0
Woosung <127220113+ws19999@users.noreply.github.com>
2024-09-13 15:23:28 +0900

    Title: 특정 조건 세 정수 비교 2, Time: 158ms, Memory: 12MB, Status: Passed - CodeTree

--- 240913/특정 조건 세 정수 비교 2/specific-comparison-of-three-natural-numbers-2.kt
+++ 240913/특정 조건 세 정수 비교 2/specific-comparison-of-three-natural-numbers-2.kt
@@ -2,7 +2,7 @@ fun main()
 {
     val (a,b,c)=readLine()!!.split(" ").map{it.toInt()}
     if(b>a && b>c)print("1 ")
-    else print("0")
+    else print("0 ")
     if(a==b && b==c)print("1")
     else print("0")
 }
