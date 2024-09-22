90880c5cd866772fd62e1ee6c671ab5e429bbb3a
Woosung <stardino99@gmail.com>
2024-09-22 11:53:23 +0900

    Title: 5와 7의 배수의 개수 세기, Time: 132ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 240922/5와 7의 배수의 개수 세기/counting-the-number-of-multiples-of-5-and-7.kt
@@ -0,0 +1,14 @@
+fun main()
+{
+    val n=readLine()!!.toInt()
+    val lists=readLine()!!.trim().split(" ").map{it.toInt()}
+    var five=0
+    var seven=0
+    for(i in 0..n-1)
+    {
+        if(lists[i]%5==0)five++
+        if(lists[i]%7==0)seven++
+    }
+    println(five)
+    println(seven)
+}
