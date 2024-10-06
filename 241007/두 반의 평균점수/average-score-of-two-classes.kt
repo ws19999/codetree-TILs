05efd98e40276156f57043e36043e1bce0daa54e
Woosung <stardino99@gmail.com>
2024-10-07 07:40:04 +0900

    Title: 두 반의 평균점수, Time: 141ms, Memory: 15MB, Status: Passed - CodeTree

--- 241007/두 반의 평균점수/average-score-of-two-classes.kt
+++ 241007/두 반의 평균점수/average-score-of-two-classes.kt
@@ -1,7 +1,7 @@
 fun main()
 {
     val n=readLine()!!.toInt()
-    var lists=readLine()!!.split(" ").map{it.toInt()}
-    val (a,b)=readLine()!!.split(" ").map{it.toInt()}
+    var lists=readLine()!!.trim().split(" ").map{it.toInt()}
+    val (a,b)=readLine()!!.trim().split(" ").map{it.toInt()}
     print(String.format("${(lists[a-1].toDouble()+lists[b-1].toDouble())/2}",.1f))
 }
