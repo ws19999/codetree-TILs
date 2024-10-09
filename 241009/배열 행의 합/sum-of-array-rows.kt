2db70a604015b7403d6e8b088cbd60c0bdc1d493
Woosung <stardino99@gmail.com>
2024-10-09 11:43:31 +0900

    Title: 배열 행의 합, Time: 145ms, Memory: 14MB, Status: Passed - CodeTree

--- 241009/배열 행의 합/sum-of-array-rows.kt
+++ 241009/배열 행의 합/sum-of-array-rows.kt
@@ -2,7 +2,7 @@ fun main()
 {
     for(i in 1..5)
     {
-        val arr=readLine()!!.split(" ").map{it.toInt()}
+        val arr=readLine()!!.trim().split(" ").map{it.toInt()}
         var ans=0
         for(j in 0..3)ans+=arr[j]
         println(ans)
