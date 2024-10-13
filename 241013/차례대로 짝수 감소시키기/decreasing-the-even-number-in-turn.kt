4147730f16c57b1565c377bcc5fb1a82f279a91b
Woosung <stardino99@gmail.com>
2024-10-13 09:42:34 +0900

    Title: 차례대로 짝수 감소시키기, Time: 92ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 241013/차례대로 짝수 감소시키기/decreasing-the-even-number-in-turn.kt
@@ -0,0 +1,16 @@
+fun main()
+{
+    var n=readLine()!!.toInt()
+    var sums=0
+    var temp=1000
+    for(i in 1..1000)
+    {
+        temp-=i*2
+        sums+=i*2
+        if(temp<=n)
+        {
+            print("${i} ${sums}")
+            break
+        }
+    }
+}
