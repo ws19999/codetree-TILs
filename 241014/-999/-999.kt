e8cd2084e18e84431b8ba8098b1758c12a65955a
Woosung <stardino99@gmail.com>
2024-10-14 07:49:19 +0900

    Title: -999, Time: 238ms, Memory: 14MB, Status: Passed - CodeTree

--- 241014/-999/-999.kt
+++ 241014/-999/-999.kt
@@ -2,7 +2,7 @@ import kotlin.math.max
 import kotlin.math.min
 fun main()
 {
-    val lists=readLine()!!.split(" ").map{it.toInt()}
+    val lists=readLine()!!.trim().split(" ").map{it.toInt()}
     var maxx=-1000
     var minn=1000
     for(i in 0..99)
