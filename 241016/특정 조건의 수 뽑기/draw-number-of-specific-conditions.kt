c2a58fe312e94ff8b581a0308deaf91648f5835d
Woosung <stardino99@gmail.com>
2024-10-16 07:19:10 +0900

    Title: 특정 조건의 수 뽑기, Time: 238ms, Memory: 16MB, Status: Passed - CodeTree

--- 241016/특정 조건의 수 뽑기/draw-number-of-specific-conditions.kt
+++ 241016/특정 조건의 수 뽑기/draw-number-of-specific-conditions.kt
@@ -7,11 +7,11 @@ fun main()
         var b=99999
         for(i in 0..n-1)
         {
-            if(abs(100-a)>abs(100-numbers[i]) || (abs(100-a)==abs(100-numbers[i]) && numbers[i]<100))
+            if(abs(100-a)>abs(100-numbers[i]) || (abs(100-a)==abs(100-numbers[i]) && numbers[i]<=100))
             {
                 a=numbers[i]
             }
-            if(numbers[i]>100 && numbers[i]<b)b=numbers[i]
+            if(numbers[i]>=100 && numbers[i]<b)b=numbers[i]
         }
         if(b==99999)b=-1
         print("${a} ${b}")
