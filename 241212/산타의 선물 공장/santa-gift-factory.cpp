e5ad80b7b481a84b5472623e463a11a88879057d
Woosung <stardino99@gmail.com>
2024-12-12 20:26:46 +0900

    Title: 산타의 선물 공장, Time: 1010ms, Memory: 9MB, Status: Time Limit Exceed - CodeTree

--- 241212/산타의 선물 공장/santa-gift-factory.cpp
+++ 241212/산타의 선물 공장/santa-gift-factory.cpp
@@ -6,11 +6,11 @@ int weights[100000];
 using namespace std;
 int main(void) {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int q,n,m,w_max,answer=0,r_id,f_id,b_num;
+	int q, n, m, w_max, answer = 0, r_id, f_id, b_num;
 	cin >> q;
-	list<pair<int,int>> belt[11];
+	list<pair<int, int>> belt[11];
 	bool available[11] = { false };
-	unordered_map<int, int> lines;
+	unordered_map<int, pair<int,list<pair<int, int>>::iterator>> keys;
 	for (int query = 1; query <= q; query++) {
 		int Q;
 		cin >> Q;
@@ -23,44 +23,47 @@ int main(void) {
 				available[i] = true;
 				for (int j = (i - 1) * n / m; j < i * n / m; j++) {
 					belt[i].push_back(make_pair(boxes[j], weights[j]));
-					lines[boxes[j]] = i;
+					auto it = belt[i].end();
+					it--;
+					keys[boxes[j]] = make_pair(i,it);
 				}
 			}
 			break;
 		case 200:
-			answer=0;
+			answer = 0;
 			cin >> w_max;
 			for (int i = 1; i <= m; i++) {
 				if (belt[i].empty())continue;
 				if (belt[i].front().second <= w_max) {
-					lines[belt[i].front().first] = 0;
+					keys[belt[i].front().first].first = 0;
 					answer += belt[i].front().second;
 				}
-				else belt[i].push_back(belt[i].front());
+				else{
+					belt[i].push_back(belt[i].front());
+					auto it = belt[i].end();
+					it--;
+					keys[belt[i].front().first].second = it;
+				}
 				belt[i].pop_front();
 			}
 			cout << answer << "\n";
 			break;
 		case 300:
 			cin >> r_id;
-			if (lines[r_id] == 0)cout << -1 << "\n";
+			if (keys[r_id].first == 0)cout << -1 << "\n";
 			else {
-				int l = lines[r_id];
-				lines[r_id] = 0;
-				auto it = belt[l].begin();
-				while ((*it).first != r_id)it++;
-				belt[l].erase(it);
+				int l = keys[r_id].first;
+				keys[r_id].first = 0;
+				belt[l].erase(keys[r_id].second);
 				cout << r_id << "\n";
 			}
 			break;
 		case 400:
 			cin >> f_id;
-			if (lines[f_id] == 0)cout << "-1\n";
+			if (keys[f_id].first == 0)cout << "-1\n";
 			else {
-				int l = lines[f_id];
-				auto it = belt[l].begin();
-				while ((*it).first != f_id)it++;
-				belt[l].splice(belt[l].begin(), belt[l], it, belt[l].end());
+				int l = keys[f_id].first;
+				belt[l].splice(belt[l].begin(), belt[l], keys[f_id].second, belt[l].end());
 				cout << l << "\n";
 			}
 			break;
@@ -69,12 +72,12 @@ int main(void) {
 			if (available[b_num] == false)cout << "-1\n";
 			else {
 				available[b_num] = false;
-				int l=b_num;
+				int l = b_num;
 				while (!available[l]) {
 					l++;
 					if (l > m)l = 1;
 				}
-				for (auto i : belt[b_num])lines[i.first] = l;
+				for (auto i : belt[b_num])keys[i.first].first = l;
 				belt[l].splice(belt[l].end(), belt[b_num]);
 				cout << b_num << "\n";
 			}
