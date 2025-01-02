e90e1948bc15f93e5a42c72cadb96c6965b703d4
Woosung <stardino99@gmail.com>
2025-01-02 14:37:24 +0900

    Title: 코드트리 채점기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250102/코드트리 채점기/codetree-judger.cpp
+++ 250102/코드트리 채점기/codetree-judger.cpp
@@ -4,25 +4,23 @@
 #include <tuple>
 #include <vector>
 using namespace std;
-unordered_map<string, int> domain; 
+unordered_map<string, int> domain;
 unordered_map<string, int> url;
 bool urls[50001];
-priority_queue<tuple<int, int, int,int>> pq; //-우선순위, -큐 시간, 도메인 id , url id
-unordered_map<int, pair<int, int>> domainhistory;
+int domainhistory[301][2];
 priority_queue<int> 채점기;
-unordered_map<int, int> 채점중;  // 채점기,도메인
-unordered_map<int, int> 채점중도메인; //도메인 1
-priority_queue<tuple<int,int, int, int>> impossible[50001]; // -우선,-큐 시간, 도메인 id, url id
-priority_queue<pair<int, int>> 가능시간; // -시간,도메인
+int 채점중[50001];  // 채점기,도메인
+int 채점중도메인[301]; //도메인 1
+priority_queue<tuple<int, int, int>> pq[301]; // -우선,-큐 시간, url id
 int domainnum = 1;
 int urlnum = 1;
 int task = 0;
 int main(void) {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int Q;
+	int Q,N=0;
 	cin >> Q;
 	for (int tc = 1; tc <= Q; tc++) {
-		int N,q;
+		int q;
 		cin >> q;
 		if (q == 100) {
 			cin >> N;
@@ -30,18 +28,18 @@ int main(void) {
 			string u0; //도메인/문제ID
 			cin >> u0;
 			url[u0] = urlnum++;
-			string str="";
+			string str = "";
 			for (auto i : u0) {
 				if (i == '/')break;
 				str += i;
 			}
 			domain[str] = domainnum++;
-			pq.push(make_tuple(-1, 0, 1,1));
+			pq[1].push(make_tuple(-1, 0, 1));
 			urls[1] = true;
 			task++;
 		}
 		else if (q == 200) {
-			int t, p,d;
+			int t, p, d;
 			string u; //정확히 일치하는 url이 하나라도 있으면 패스.
 			cin >> t >> p >> u;
 			if (urls[url[u]])continue;
@@ -60,10 +58,7 @@ int main(void) {
 			}
 			else d = domain[str];
 			urls[urln] = true;
-			if (채점중도메인[d] or t<3*domainhistory[d].second-2*domainhistory[d].first) {
-				impossible[d].push(make_tuple(-p, -t, d, urln));
-			}
-			else pq.push(make_tuple(-p, -t, d, urln));
+			pq[d].push(make_tuple(-p, -t, urln));
 		}
 		else if (q == 300) { //채점 가능한 것 중에서 우선순위가 가장 높은 task 채점.
 			//채점 진행중인 도메인 있으면 패스. 일치 도메인 주에서 start + 3*gap 보다 작으면 패스.
@@ -71,26 +66,23 @@ int main(void) {
 			//번호가 가장 작은 것이 우선순위 채점 시작.
 			int t;
 			cin >> t;
-			while (!가능시간.empty()) {
-				if (-가능시간.top().first <= t) {
-					int dnum = 가능시간.top().second;
-					가능시간.pop();
-					if (!impossible[dnum].empty()) {
-						pq.push(impossible[dnum].top());
-						impossible[dnum].pop();
-					}
+			int nextone=0,priority = 99999999,qt=99999999;
+			for (int i = 1; i <domainnum; i++) {
+				if (채점중도메인[i] or t < domainhistory[i][0] + 3 * domainhistory[i][1] or pq[i].empty())continue;
+				if (priority > -get<0>(pq[i].top()) or (priority == -get<0>(pq[i].top()) and qt > -get<1>(pq[i].top()))) {
+					nextone = i;
+					priority = -get<0>(pq[i].top());
+					qt = -get<1>(pq[i].top());
 				}
-				else break;
 			}
-			if(!채점기.empty() and !pq.empty()) {
-				int dnum = get<2>(pq.top());
-				urls[get<3>(pq.top())]=false;
+			if (!채점기.empty() and nextone) {
+				urls[get<2>(pq[nextone].top())] = false;
 				int chae = -채점기.top();
 				채점기.pop();
-				채점중[chae] = dnum;
-				domainhistory[dnum].first = t;
-				채점중도메인[dnum] = 1;
-				pq.pop();
+				채점중[chae] = nextone;
+				domainhistory[nextone][0] = t;
+				채점중도메인[nextone] = 1;
+				pq[nextone].pop();
 				task--;
 			}
 		}
@@ -101,9 +93,8 @@ int main(void) {
 				int dnum = 채점중[J_id];
 				채점중[J_id] = 0;
 				채점중도메인[dnum] = 0;
-				domainhistory[dnum].second = t;
+				domainhistory[dnum][1] = t - domainhistory[dnum][0];
 				채점기.push(-J_id);
-				가능시간.push(make_pair(-3 * t + 2*domainhistory[dnum].first, dnum));
 			}
 		}
 		else {
@@ -111,6 +102,7 @@ int main(void) {
 			cin >> t;
 			cout << task << "\n";
 		}
+
 	}
 	return 0;
 }
