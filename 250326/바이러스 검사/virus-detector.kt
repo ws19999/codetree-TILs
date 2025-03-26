cb2fab579363f4c4c4f478c34e5c02ef5bcdd3ba
Woosung <stardino99@gmail.com>
2025-03-26 22:05:23 +0900

    Title: 바이러스 검사, Time: 133ms, Memory: 12MB, Status: Runtime Error - Codetree

---
+++ 250326/바이러스 검사/virus-detector.kt
@@ -0,0 +1,18 @@
+fun main()
+{
+    val br=System.`in`.bufferedReader()
+    val bw=System.`out`.bufferedWriter()
+    val N=br.readLine().toInt()
+    var ans=N.toLong()
+    val guests=br.readLine().split(" ").map{it.toInt()}.toIntArray()
+    val (a,b)=br.readLine().split(" ").map{it.toInt()}
+    for(i in 0..N-1){
+        guests[i]-=a
+        if(guests[i]>0){
+            ans+=guests[i]/b
+            if(guests[i]%b>0)ans++
+        }
+    }
+    bw.write("$ans")
+    bw.close()
+}
